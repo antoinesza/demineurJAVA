d16a0e6211208267c6b4c19906e07c1dd894e18d
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-06 15:33:20 +0100

    Add class MouseListener

---
+++ src/mouseListener.java
@@ -0,0 +1,20 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class mouseListener implements MouseListener {
+
+
+    public void mouseReleased(MouseEvent arg0){
+
+    }
+    public void mouseReleased(MouseEvent arg0){
+
+    }
+    public void mouseReleased(MouseEvent arg0){
+
+    }
+    public void mouseReleased(MouseEvent arg0){
+
+    }
+}
