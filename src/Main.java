75ac9213a1d23985b0da6d3cf8ef409ab78a88fe
Tao BERQUER <tao.berquer@gmail.com>
2019-01-08 15:18:31 +0100

    Le mode graphique n'est plus le premier à se lancer. Un choix un disponible au lancement.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Frame frame = new Frame("DÃ©mineur");
-        TimerThread Timer = new TimerThread();
-        Timer.start();
+    	Scanner sc = new Scanner(System.in);
+    	String str = "";
+        do {
+        	System.out.println("Pour accèder à la version console du démineur taper :        'console'");
+        	System.out.println("Pour accèder à la version graphique du démineur taper :      'graphique'");
+        	System.out.println("Pour quitter le programme taper :                            'quitter'");
+        	
+        	str = sc.nextLine();
+        	
+        	switch (str) {
+				case "graphique":
+					Frame frame = new Frame("Démineur");
+					TimerThread Timer = new TimerThread();
+					Timer.start();
+					str = "quitter";
+					break;  
+				
+				case "console":
+					System.out.println("Hello world!");
+					break;
+        	}
+        }while(!str.equals("quitter"));
+        sc.close();
     }
 }
